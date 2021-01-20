908730b8b68dee91361d7a34ccb042c67d44f5fb
BananZGan <bananz@live.com>
2021-01-20 15:37:09 +0800

    1 file modified, 2 added, 1 deleted

--- src/Components/ImageUpload.js
+++ src/Components/ImageUpload.js
@@ -1,115 +1,80 @@
 // imports the React Javascript Library
 import React, { useState } from "react";
 //Card
-import { Card, Container, Button, Fab, Grid } from "@material-ui/core";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-
-import { blue } from "@material-ui/core/colors";
-
-import AddPhotoAlternateIcon from "@material-ui/icons/AddPhotoAlternate";
+import { Button, Grid } from "@material-ui/core";
+import background from "../resources/background";
+import face from "../resources/face";
 
 //Tabs
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
+  imageBox: {
+    height: 100,
+    margin: 20,
+    width: 100,
+    borderRadius: "50%",
+    boxShadow: "0 0 5px red",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "50%",
+    backgroundSize: "60%",
+    backgroundColor: "#fff"
+  },
+  imageBox2: {
+    height: "100%",
+    width: "100%",
+    background: "#fff",
+    borderRadius: "50%",
+    overflow: "hidden"
   },
   submit: {
     margin: theme.spacing(3, 0, 2)
   },
-  icon: {
-    margin: theme.spacing(2)
-  },
   input: {
     display: "none"
-  },
-  button: {
-    color: blue[900],
-    margin: 10
   }
 }));
 
 export default function ImageUploadCard({ onImageUpload }) {
   const classes = useStyles();
   const [selectedImage, setSelectedImage] = useState(null);
-  const [selectedFile, setSelectedFile] = useState(null);
 
   const handleUploadClick = event => {
     var file = event.target.files[0];
+    if (!file) {
+      return;
+    }
     const reader = new FileReader();
     reader.readAsDataURL(file);
-
     reader.onloadend = function(e) {
       setSelectedImage(reader.result);
+      onImageUpload(reader.result);
     }.bind(this);
-    setSelectedFile(file);
-  };
-
-  const onCapture = file => {
-    setSelectedImage(file);
-    setSelectedFile(new File([file], "selfie.jpeg"));
-  };
-
-  const renderInitialState = () => {
-    return (
-      <React.Fragment>
-        <CardContent>
-          <Grid container justify="center" alignItems="center">
-            <input
-              accept="image/*"
-              className={classes.input}
-              id="contained-button-file"
-              type="file"
-              onChange={handleUploadClick}
-            />
-            <label htmlFor="contained-button-file">
-              <Fab component="span" className={classes.button}>
-                <AddPhotoAlternateIcon />
-              </Fab>
-            </label>
-          </Grid>
-        </CardContent>
-      </React.Fragment>
-    );
-  };
-
-  const imageResetHandler = event => {
-    setSelectedFile(null);
-    setSelectedImage(null);
-  };
-
-  const renderUploadedState = () => {
-    return (
-      <React.Fragment>
-        <CardActionArea onClick={imageResetHandler}>
-          <img width="100%" className={classes.media} src={selectedImage} />
-          <label>Tap to remove</label>
-        </CardActionArea>
-      </React.Fragment>
-    );
   };
 
   return (
-    <React.Fragment>
-      <Container className={classes.form}>
-        <Card>
-          {(!!selectedImage && renderUploadedState()) || renderInitialState()}
-        </Card>
-        <Button
-          type="submit"
-          disabled={!selectedFile}
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-          onClick={() => onImageUpload(selectedFile)}
-        >
-          Upload
-        </Button>
-      </Container>
-    </React.Fragment>
+    <Grid container justify="center" alignItems="center">
+      <Grid item>
+        <div className={clases.imageBox}>
+          <div className={classes.imageBox2}>
+            <img src={selectedImage ?? face} />
+          </div>
+        </div>
+      </Grid>
+      <Grid item>
+        <input
+          accept="image/*"
+          className={classes.input}
+          id="contained-button-file"
+          type="file"
+          onChange={handleUploadClick}
+        />
+        <label htmlFor="contained-button-file">
+          <Button component="span" variant="contained" color="primary">
+            SUBMIT a selfie
+          </Button>
+        </label>
+      </Grid>
+    </Grid>
   );
 }
